4503790ad1eb80738393a180c1ea97d2421c5c15
vinay <vinay@DESKTOP-TM236BS>
2021-05-09 15:43:52 +0530

    chek this new experement

---
+++ com.sample/src/main/java/com/sample/productTest/Closuretest.java
@@ -0,0 +1,12 @@
+package com.sample.productTest;
+
+import org.testng.annotations.Test;
+
+public class Closuretest {
+	
+	@Test
+	public void testing() {
+		System.out.println("This is new experemental");
+	}
+
+}
