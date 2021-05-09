64211428549709cc5dd4f0b4349c800618bdd715
vinay <vinay@DESKTOP-TM236BS>
2021-05-09 15:24:33 +0530

    comiting now

---
+++ com.sample/src/main/java/com/sample/productTest/Classictest.java
@@ -0,0 +1,12 @@
+package com.sample.productTest;
+
+import org.testng.annotations.Test;
+
+public class Classictest {
+	
+	@Test
+	public void testing() {
+		System.out.println("This is experemental");
+	}
+
+}
